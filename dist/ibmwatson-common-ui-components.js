16509ce2ecde1aafa3d7940616ce06a85e7a086f
Joe Pavitt <joepavitt@joes-mbp.hursley.uk.ibm.com>
2015-09-21 12:19:35 +0100

    Defect 45866 - Update /dist folder with a grunt build

--- dist/ibmwatson-common-ui-components.js
+++ dist/ibmwatson-common-ui-components.js
@@ -211,7 +211,7 @@ angular.module('watsonAlerts/watsonAlertsBar.html', []).run(['$templateCache', f
     '		<button ng-show="alert.dismissable" ng-click="alert.dismiss()" type="button" class="close ibm-alert__close" data-dismiss="alert" aria-label="Close">\n' +
     '			<span class="ibm-icon--close-cancel-error" aria-hidden="true"></span>\n' +
     '		</button>\n' +
-    '		<span ng-show="alert.title"><strong>{{alert.title}}</strong> </span>{{alert.text}}<span ng-show="alert.link"> <a href="{{alert.link}}">{{alert.linkText || \'Learn more\'}}</a></span>\n' +
+    '		<span ng-show="alert.title"><strong>{{alert.title}}</strong> </span>{{alert.text || \'Unknown Error Occurred\' }}}}<span ng-show="alert.link"> <a href="{{alert.link}}">{{alert.linkText || \'Learn more\'}}</a></span>\n' +
     '	</div>\n' +
     '</div>\n' +
     '');
